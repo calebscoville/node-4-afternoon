3a184214205a4c286d31a013d0f2454f70790374
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-21 15:39:53 -0600

    sixth

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const checkForSession = require("./middlewares/checkForSession");
 const swagController = require("./controllers/swagController");
+const authController = require('./controllers/authController');
 
 const app = express();
 
@@ -17,6 +18,11 @@ app.use(
     })
 );
 app.use(checkForSession);
+
+app.post("/api/register", authController.register);
+app.post("/api/login", authController.login);
+app.post("/api/signout", authController.signout);
+app.get("/api/user", authController.getUser);
 
 app.get("/api/swag", swagController.read);
 
